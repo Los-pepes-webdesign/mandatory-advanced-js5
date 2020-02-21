98e3110b04e8b34c945fe58154807edfa4418229
Hampus Olsen <hampus.olsen@outlook.com>
2020-02-21 14:29:05 +0100

    Added connect to dropbox feature.

--- src/components/pages/login/Main.js
+++ src/components/pages/login/Main.js
@@ -1,15 +1,23 @@
-import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
 import React from 'react';
+import { Dropbox } from 'dropbox';
+
+import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
+
+const dropbox = new Dropbox({
+	clientId : 'wwft9hg3g9qhuth',
+	fetch
+});
+
 export default function Main() {
 	return (
-    <main className="login__main">
-    <div className="login__main__buttonContainer">
-      <div className="login__main__button">
-        <div tabIndex='0' className="login__main__icon">
-         <ArrowForwardIcon />
-        </div>
-      </div>
-    </div>
-    </main>
+		<main className='login__main'>
+			<a href={dropbox.getAuthenticationUrl('http://localhost:3000/')} className='login__main__buttonContainer'>
+				<div className='login__main__button'>
+					<div tabIndex='0' className='login__main__icon'>
+						<ArrowForwardIcon />
+					</div>
+				</div>
+			</a>
+		</main>
 	);
 }
