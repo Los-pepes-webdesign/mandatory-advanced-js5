a35b1ea4f17f4c30ffb411c9a73f96fc61c46c16
Hampus Olsen <hampus.olsen@outlook.com>
2020-02-21 11:10:44 +0100

    Added redirect from main to login on invalid token.

--- src/components/pages/main/Main.js
+++ src/components/pages/main/Main.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { Redirect } from 'react-router';
 
 export default function Main() {
+	if (window.location.hash.length < 2) return <Redirect to='/login' />;
 	return <main />;
 }
