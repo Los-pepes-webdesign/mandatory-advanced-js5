d3840dd3f2e86cb275535d3fbffe8cc47f23b4ba
Pjudd1 <oskar.hertzman@hotmail.com>
2020-02-21 12:55:59 +0100

    added login button

--- src/components/pages/login/Header.js
+++ src/components/pages/login/Header.js
@@ -1,13 +1,8 @@
 import React from 'react';
-
 export default function Header() {
 	return (
-		<>
 		<header className="login__header">
-		<h1>Pepebox</h1>
+		
 		</header>
-		<main className="login__main">
-		</main>
-		</>
 	);
 }
