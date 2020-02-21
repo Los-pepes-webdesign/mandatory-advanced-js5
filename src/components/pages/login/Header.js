8aec55cae3508fdc753c3866bd811db72c69704d
Pjudd1 <oskar.hertzman@hotmail.com>
2020-02-21 11:07:20 +0100

    fixed import of style variables

--- src/components/pages/login/Header.js
+++ src/components/pages/login/Header.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
 export default function Header() {
-	return <div />;
+	return (
+		<header className="login__header">
+		<h1>Pepebox</h1>
+		</header>
+	);
 }
--- src/components/pages/login/Login.js
+++ src/components/pages/login/Login.js
@@ -1,5 +1,11 @@
 import React from 'react';
+import Header from './Header';
 
 export default function Login() {
-	return <div className='App' />;
+	return (
+
+		<Header />
+
+	);
+
 }
