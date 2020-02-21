4d728a5802c84fad1a571f8ceb37851ca32d716b
Jesper Bengtsson Ekwall <jeppen@live.com>
2020-02-21 12:32:52 +0100

    Updated footer with text

--- src/components/pages/login/Footer.js
+++ src/components/pages/login/Footer.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
 export default function Footer() {
-	return <div />;
+	return <footer>
+					<div className="footer__line"></div>
+					<p className="footer__copyright">Los Pepes © 2020</p>
+				</footer>;
 }
