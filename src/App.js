f21928632a2eb47f2500c58b9d549bafc4825287
Hampus Olsen <hampus.olsen@outlook.com>
2020-02-21 10:12:18 +0100

    Fixed boilerplate.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
 import React from 'react';
 import Main from './components/pages/Main.js';
 import Login from './components/pages/Login.js';
-import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
-import logo from './logo.svg';
-import './styles/App.css';
+import './styles/App.scss';
 
 function App() {
-
-  let redirect;
-  if (window.location.pathname === "/") {
-    redirect = <Redirect to="/login" />
-  }
-  return (
-    <div className="App">
-      <HelmetProvider>
-        <Router>
-          {redirect}
-          <Route path="/login" component={Login} />
-          <Route path="/home" component={Main} />
-      </Router>
-      </HelmetProvider>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<HelmetProvider>
+				<Router>
+					<Route path='/login' component={Login} />
+					<Route path='/' component={Main} />
+				</Router>
+			</HelmetProvider>
+		</div>
+	);
 }
 
 export default App;
