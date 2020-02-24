2874affd6586aa41b79366dab11794a280d91a18
Pjudd1 <oskar.hertzman@hotmail.com>
2020-02-24 11:07:41 +0100

    fetching all files from account

--- src/components/pages/main/Main.js
+++ src/components/pages/main/Main.js
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, {useState, useReducer, useEffect} from 'react';
 import { Redirect } from 'react-router';
-
+import { Dropbox } from 'dropbox';
 import Mainmenu from './Mainmenu';
 import Header from './Header';
 import Content from './Content';
 import Profile from './Profile';
 
-const regex = new RegExp(/=(.*)(?=&token_type)/, 'i');
-const token = window.location.hash.match(regex)[1];
+const dropbox = new Dropbox({
+	clientId : 'wwft9hg3g9qhuth',
+	fetch
+	});
 
 export default function Main() {
-	if (window.location.hash.length < 2) return <Redirect to='/login' />;
+	useEffect(() => {
+		getAllFiles();
+	}, [])
 
+	if (window.location.hash.length < 2) return <Redirect to='/login' />;
+		const regex = new RegExp(/=(.*)(?=&token_type)/, 'i');
+		const token = window.location.hash.match(regex)[1];
+
+		function getAllFiles () {
+			dropbox.setAccessToken(token);
+			dropbox.filesListFolder({
+			path: ''
+		}).then(response =>
+			console.log(response));
+		}
 	return (
 		<div className='main'>
 			<Mainmenu />
