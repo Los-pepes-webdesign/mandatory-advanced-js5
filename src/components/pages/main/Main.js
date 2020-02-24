549ba569cd0112cc8b36d8723cd8ef5ac72f2ff7
Hampus Olsen <hampus.olsen@outlook.com>
2020-02-24 10:31:56 +0100

    Added basic grid layout for main and regex'd token.

--- src/components/pages/main/Main.js
+++ src/components/pages/main/Main.js
@@ -1,7 +1,23 @@
 import React from 'react';
 import { Redirect } from 'react-router';
 
+import Mainmenu from './Mainmenu';
+import Header from './Header';
+import Content from './Content';
+import Profile from './Profile';
+
+const regex = new RegExp(/=(.*)(?=&token_type)/, 'i');
+const token = window.location.hash.match(regex)[1];
+
 export default function Main() {
 	if (window.location.hash.length < 2) return <Redirect to='/login' />;
-	return <main />;
+
+	return (
+		<div className='main'>
+			<Mainmenu />
+			<Header />
+			<Profile />
+			<Content />
+		</div>
+	);
 }
