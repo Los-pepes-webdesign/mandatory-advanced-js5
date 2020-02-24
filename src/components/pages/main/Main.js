fbada66c34001f4a1efc9b69becbad07bf8169bc
Pjudd1 <oskar.hertzman@hotmail.com>
2020-02-24 11:11:48 +0100

    Revert "fetching all files from account"
    This reverts commit 2874affd6586aa41b79366dab11794a280d91a18.

--- src/components/pages/main/Main.js
+++ src/components/pages/main/Main.js
@@ -1,32 +1,17 @@
-import React, {useState, useReducer, useEffect} from 'react';
+import React from 'react';
 import { Redirect } from 'react-router';
-import { Dropbox } from 'dropbox';
+
 import Mainmenu from './Mainmenu';
 import Header from './Header';
 import Content from './Content';
 import Profile from './Profile';
 
-const dropbox = new Dropbox({
-	clientId : 'wwft9hg3g9qhuth',
-	fetch
-	});
+const regex = new RegExp(/=(.*)(?=&token_type)/, 'i');
+const token = window.location.hash.match(regex)[1];
 
 export default function Main() {
-	useEffect(() => {
-		getAllFiles();
-	}, [])
-
 	if (window.location.hash.length < 2) return <Redirect to='/login' />;
-		const regex = new RegExp(/=(.*)(?=&token_type)/, 'i');
-		const token = window.location.hash.match(regex)[1];
 
-		function getAllFiles () {
-			dropbox.setAccessToken(token);
-			dropbox.filesListFolder({
-			path: ''
-		}).then(response =>
-			console.log(response));
-		}
 	return (
 		<div className='main'>
 			<Mainmenu />
