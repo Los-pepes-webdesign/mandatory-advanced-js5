945c347871b722b9a61075879c9bd486615236cb
Hampus Olsen <hampus.olsen@outlook.com>
2020-02-24 17:30:27 +0100

    Fixed initiation bug.

--- src/components/pages/main/Main.js
+++ src/components/pages/main/Main.js
@@ -6,11 +6,11 @@ import Header from './Header';
 import Content from './Content';
 import Profile from './Profile';
 
-const regex = new RegExp(/=(.*)(?=&token_type)/, 'i');
-const token = window.location.hash.match(regex)[1];
-
 export default function Main() {
 	if (window.location.hash.length < 2) return <Redirect to='/login' />;
+
+	const regex = new RegExp(/=(.*)(?=&token_type)/, 'i');
+	const token = window.location.hash.match(regex)[1];
 
 	return (
 		<div className='main'>
