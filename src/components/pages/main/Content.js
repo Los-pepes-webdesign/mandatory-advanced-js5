9883bb36c316fa8630bdb93f1d8be22978f9eb2b
Hampus Olsen <hampus.olsen@outlook.com>
2020-02-26 10:03:25 +0100

    Moved logic to store.js.

--- src/components/pages/main/Content.js
+++ src/components/pages/main/Content.js
@@ -1,5 +1,32 @@
 import React from 'react';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import { useObservable, state$ } from '../../../utilities/store';
 
 export default function Content() {
-	return <main className='content' />;
+	const { files } = useObservable(state$);
+
+	return (
+		<main className='content'>
+			<table>
+				<thead>
+					<tr>
+						<th>Name</th>
+						<th>Modified</th>
+						<th />
+					</tr>
+				</thead>
+				<tbody>
+					{files.map((file) => (
+						<tr key={file.id}>
+							<td>{file.name}</td>
+							<td>{file.server_modified}</td>
+							<td>
+								<MoreVertIcon />
+							</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+		</main>
+	);
 }
--- src/components/pages/main/Main.js
+++ src/components/pages/main/Main.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Redirect } from 'react-router';
+import { dropbox, setState$ } from '../../../utilities/store';
 
 import Mainmenu from './Mainmenu';
 import Header from './Header';
@@ -7,17 +8,41 @@ import Content from './Content';
 import Profile from './Profile';
 
 export default function Main() {
-	if (window.location.hash.length < 2) return <Redirect to='/login' />;
+	const [ invalidHash, setInvalidHash ] = useState(false);
 
-	const regex = new RegExp(/=(.*)(?=&token_type)/, 'i');
-	const token = window.location.hash.match(regex)[1];
+	useEffect(() => {
+		if (window.location.hash.length < 2) {
+			setInvalidHash(true);
+		} else {
+			const regex = new RegExp(/=(.*)(?=&token_type)/, 'i');
+			const token = window.location.hash.match(regex)[1];
+			dropbox.setAccessToken(token);
+			dropbox.filesListFolder({ path: '' }).then(({ entries }) => {
+				const folders = entries.filter((file) => file['.tag'] === 'folder');
+				const files = entries.filter((file) => file['.tag'] === 'file');
+				const promises = files.map((file) => dropbox.filesGetTemporaryLink({ path: file.path_lower }));
+				Promise.all(promises).then((result) => {
+					setState$(
+						[ ...folders, ...result.map((path, index) => ({ ...files[index], href: path.link })) ],
+						'setFiles'
+					);
+				});
+			});
+			dropbox.usersGetCurrentAccount().then((response) => {
+				setState$(response, 'setProfile');
+			});
+		}
+	}, []);
 
 	return (
-		<div className='main'>
-			<Mainmenu />
-			<Header />
-			<Profile />
-			<Content />
-		</div>
+		<React.Fragment>
+			{invalidHash && <Redirect to='/login' />}
+			<div className='main'>
+				<Mainmenu />
+				<Header />
+				<Profile />
+				<Content />
+			</div>
+		</React.Fragment>
 	);
 }
