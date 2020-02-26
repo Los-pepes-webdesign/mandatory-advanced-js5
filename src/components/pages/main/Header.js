89a531580ce8d1612765077cd826e5872b3d4046
Daniel Westin <DanielWestin@Daniels-MacBook-Pro.local>
2020-02-26 08:47:19 +0100

    Added Searchbar

--- src/components/pages/main/Header.js
+++ src/components/pages/main/Header.js
@@ -1,5 +1,16 @@
 import React from 'react';
+import SearchIcon from '@material-ui/icons/Search';
 
 export default function Header() {
-	return <header className='header' />;
+	return (
+		<header className="header">
+			<form className="header__form">
+				<input className="header__form__input" type="text" placeholder="search" />
+
+				<button className="header__form__button" name="button">
+					<SearchIcon style={{ color: '#900C3F' }} />
+				</button>
+			</form>
+		</header>
+	);
 }
