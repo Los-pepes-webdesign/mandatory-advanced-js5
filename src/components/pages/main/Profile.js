cfdaf9152a3e6ec906f8999259650cada4ae2960
Jesper Bengtsson Ekwall <jeppen@live.com>
2020-02-26 13:26:03 +0100

    Mockup Profile.js ProfileMore,js

--- src/components/pages/main/Profile.js
+++ src/components/pages/main/Profile.js
@@ -1,23 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AccountBoxIcon from '@material-ui/icons/AccountBox';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import { useObservable, state$ } from '../../../utilities/store';
+import ProfileMore from "./ProfileMore";
+
 
 export default function Profile() {
 	const { profile } = useObservable(state$);
+	const [showMore, updateShowMore] = useState(false);
 
 	if (!profile.name) return null;
 
 	return (
 		<div className='profile'>
 			<AccountBoxIcon />
-			<div className='profile__information'>
-				<div className='greeting'>Hola, {profile.name.display_name}</div>
-				<div className='email'>{profile.email}</div>
+			<div className='profile__info'>
+				<div className="profile__info__userAvatar">[AvatarIcon]</div>
+				<div className='profile__info__greeting'>Hola, {profile.name.given_name}!</div>
 			</div>
-			<div className='profile__more'>
-				<MoreVertIcon />
-			</div>
+			<button className="profile__info__moreButton" onClick={() => updateShowMore(!showMore)}><MoreVertIcon /></button>
+			{ showMore ? <ProfileMore profile={profile}/> : null }
 		</div>
 	);
 }
