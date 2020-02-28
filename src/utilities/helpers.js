88c9c5d05f50fee41a51e5d222058ff60179768e
Jesper Bengtsson Ekwall <jeppen@live.com>
2020-02-28 11:20:06 +0100

    _helper.js refined userSpace function

--- src/utilities/helpers.js
+++ src/utilities/helpers.js
@@ -1,34 +1,34 @@
 // FUNCTION: usedSpaceFormatting(), return value of currently used space on PepesBox formatted to two decimals with suffix (bytes, KB, MB, GB)
-export function usedSpaceFormatting(userSpace) {
+export function usedSpaceFormatting(value) {
 	const kb = 1e3;
 	const mb = 1e6;
 	const gb = 1e9;
 	const tb = 1e12;
 
-	if (userSpace.used < kb) {
-		return userSpace.used + ' bytes';
+	if (value < kb) {
+		return value + ' bytes';
 	}
-	else if (userSpace.used < mb && userSpace.used > kb) {
-		let decimalized = userSpace.used / kb;
+	else if (value < mb && value > kb) {
+		let decimalized = value / kb;
 		let formatted = Math.round((decimalized + Number.EPSILON) * 100) / 100 + ' KB';
 		return formatted;
 	}
-	else if (userSpace.used < gb && userSpace.used > mb) {
-		let decimalized = userSpace.used / mb;
+	else if (value < gb && value > mb) {
+		let decimalized = value / mb;
 		let formatted = Math.round((decimalized + Number.EPSILON) * 100) / 100 + ' MB';
 		return formatted;
 	}
-	else if (userSpace.used < tb && userSpace.used > gb) {
-		let decimalized = userSpace.used / gb;
+	else if (value < tb && value > gb) {
+		let decimalized = value / gb;
 		let formatted = Math.round((decimalized + Number.EPSILON) * 100) / 100 + ' GB';
 		return formatted;
 	}
 }
 
 // FUNCTION: maxSpaceFormatting(), returns value of the currently max storage space of PepesBox formatted to integral with suffix GB
-export function maxSpaceFormatting(userSpace) {
+export function maxSpaceFormatting(value) {
 	const gb = 1e9;
-	const maxSpaceDecimalized = userSpace.allocation.allocated / gb;
+	const maxSpaceDecimalized = value / gb;
 	const maxSpaceFormatted = Math.round((maxSpaceDecimalized + Number.EPSILON) * 1) / 1 + ' GB';
 	return maxSpaceFormatted;
 }
